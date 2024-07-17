8bf467f6c76c5aed4af1cfd9777560f5eec6ca46
Chasse-Poulet <thomas.bauque@gmail.com>
2024-07-17 11:52:57 +0200

    changed secret key variable name

--- src/modules/cart/cart.controller.js
+++ src/modules/cart/cart.controller.js
@@ -1,6 +1,6 @@
 const CartService = require("./cart.service");
 const OrderService = require("../orders/order.service");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY_TEST);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.addToCart = async (req, res) => {
   try {
